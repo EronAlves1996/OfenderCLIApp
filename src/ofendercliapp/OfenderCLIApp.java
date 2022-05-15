d056de88bba350d94eeb580a3f1b6d3e328bf96e
Eron Alves <eron.alves@rocketmail.com>
2022-05-15 15:26:20 -0300

    Added more valid arguments

--- src/ofendercliapp/OfenderCLIApp.java
+++ src/ofendercliapp/OfenderCLIApp.java
@@ -17,6 +17,10 @@ public class OfenderCLIApp {
         for (String offense : args) {
             if (offense.equals("-f")) System.out.println("Fuck you");
             else if (offense.equals("-a")) System.out.println("Asshole");
+            else if (offense.equals("-b")) System.out.println("Bastard");
+            else if (offense.equals("-m")) System.out.println("Motherfucker");
+            else if (offense.equals("-i")) System.out.println("Idiot");
+            else if (offense.equals("-s")) System.out.println("Suck my dick!!");
         }
     }
     
