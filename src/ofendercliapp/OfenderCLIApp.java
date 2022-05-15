c40824d5e61ea3e4bc98415ca8aefb3db9ba78c8
Eron Alves <eron.alves@rocketmail.com>
2022-05-15 14:39:50 -0300

    Define basic comments and docs

---
+++ src/ofendercliapp/OfenderCLIApp.java
@@ -0,0 +1,18 @@
+package ofendercliapp;
+
+/**
+ *
+ * @author Eron Alves
+ * This class initialize the simple ofender
+ */
+public class OfenderCLIApp {
+
+    /**
+     * @param args Define the offenses the program will print in command line
+     */
+    public static void main(String[] args) {
+            
+        }
+    }
+    
+}
